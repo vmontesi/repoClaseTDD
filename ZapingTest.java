1ee31dfdd22c8275aac358712a4df1d2b11c46f9
Victor <victormontesinosarenas@gmail.com>
2023-04-25 15:42:33 +0200

    Test canal origen: 10 canal destino 30: Salida: 20 clics

--- ZapingTest.java
+++ ZapingTest.java
@@ -5,13 +5,21 @@ public class ZapingTest {
     @org.junit.jupiter.api.Test
     public void testCanalOrigen1() {
         Zapping zapping = new Zapping();
-        int clics = zapping.calcularClics(0,0);
+        int clics = zapping.calcularClics(0, 0);
         assertEquals(0, clics);
     }
+
     @org.junit.jupiter.api.Test
     public void testCanalOrigen2() {
         Zapping zapping = new Zapping();
-        int clics = zapping.calcularClics(1,2);
+        int clics = zapping.calcularClics(1, 2);
         assertEquals(1, clics);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testCanalOrigen3() {
+        Zapping zapping = new Zapping();
+        int clics = zapping.calcularClics(10, 30);
+        assertEquals(20, clics);
     }
 }
