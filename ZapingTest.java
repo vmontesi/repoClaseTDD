8b9ffe9472a769258c264a602d615a18b91da73e
Victor <victormontesinosarenas@gmail.com>
2023-04-25 15:40:10 +0200

    6. Test canal origen: 1 canal destino 2: Salida: 1 clics → Commit

--- ZapingTest.java
+++ ZapingTest.java
@@ -8,4 +8,10 @@ public class ZapingTest {
         int clics = zapping.calcularClics(0,0);
         assertEquals(0, clics);
     }
+    @org.junit.jupiter.api.Test
+    public void testCanalOrigen2() {
+        Zapping zapping = new Zapping();
+        int clics = zapping.calcularClics(1,2);
+        assertEquals(1, clics);
+    }
 }
